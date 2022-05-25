e2fe72999c76f8215df7c65ed4b026d584a560c3
Hannah Mathot <hannah.mathot@laposte.net>
2022-05-26 00:21:04 +0200

    landing layout v1

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,25 +1,5 @@
 "use strict";
 
-//slider range normal
-//source : https://www.youtube.com/watch?v=YE10hytBBho
-let indexValue = document.querySelector(".section-4__range-value");
-let indexRange = document.querySelector(".section-4__range-barre");
-
-indexRange.addEventListener("input", indexSlider);
-
-function indexSlider(e){
-    let currentValue = e.currentTarget;
-    if (currentValue.value == 0) {
-        indexValue.innerHTML = "Ecologiques";
-    } 
-    if (currentValue.value == 1){
-        indexValue.innerHTML = "Moins chères que leur équivalent en grandes-surfaces";
-    }
-    if (currentValue.value == 2){
-        indexValue.innerHTML = "Efficaces";
-    }
-}
-
 // Année (Copyright)
 var annee = document.querySelector(".copyright__annee");
 let date = new Date();
