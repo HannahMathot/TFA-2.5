5b462e9a73ac2804e07c9d1d2c9cbbd00da12eb9
Hannah Mathot <hannah.mathot@laposte.net>
2022-05-31 09:18:25 +0200

    changement DA, JS cleaned, grid

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,7 +1,9 @@
 "use strict";
 
 // Année (Copyright)
-var annee = document.querySelector(".copyright__annee");
+let annee = document.querySelector(".copyright__annee");
 let date = new Date();
 let year = date.getUTCFullYear();
 annee.innerHTML = year
+
+//Animations
